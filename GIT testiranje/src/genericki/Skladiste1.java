2d30028b9ccb66026da5b1866b8df4f4b1499238
toka88 <tokan988@gmail.com>
2015-12-26 12:54:20 +0100

    Dodat je novi folder

---
+++ GIT testiranje/src/genericki/Skladiste1.java
@@ -0,0 +1,15 @@
+package genericki;
+
+class Automobil{}
+
+public class Skladiste1 {
+	private Automobil a;
+	public Skladiste1(Automobil a){this.a = a;}
+	public Automobil get(){return a;}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ GIT testiranje/src/genericki/Skladiste3.java
@@ -0,0 +1,16 @@
+package genericki;
+
+public class Skladiste3<T> {
+	private T a;
+	public Skladiste3(T a){this.a = a;}
+	public void set(T a){this.a = a;}
+	public T get(){return a;}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Skladiste3<Automobil> bmw = new Skladiste3<Automobil>(new Automobil());
+		Automobil mercedes = bmw.get();		// Konverzija tipa nije potrebna
+		
+	}
+
+}
